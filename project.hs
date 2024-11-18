2c8418b40880cafdba2d2a3ad7c16e63167ee13d
KassiyetB <kaszi.exe@gmail.com>
2024-11-18 22:22:40 +0500

    clean project

--- project.hs
+++ project.hs
@@ -1,116 +1,140 @@
--- Haskell Library Membership Management System
-
-module LibrarySystem where
-
-import Data.Map (Map)
-import qualified Data.Map as Map
-import Data.List (sortOn, group, maximumBy)
+import Data.Time
+import qualified  Data.Map as Map
 import Data.Maybe (isNothing)
-import Data.Time (Day)
 
--- Data Structures
+data MembershipType = Standard | Premium deriving (Show, Eq, Ord)
 
-data MembershipType = Standard | Premium deriving (Show, Eq)
-
-data Member = Member
-    { memberId :: Int
-    , name :: String
-    , membershipType :: MembershipType
-    , email :: String
+data Member = Member {
+    memberId :: Int,
+    name :: String,
+    membershipType :: MembershipType,
+    email :: String
     } deriving (Show)
 
-
-data LoanRecord = LoanRecord
-    { bookTitle :: String
-    , author :: String
-    , loanDate :: Day
-    , returnDate :: Maybe Day
-    , memberInfo :: Member
+data Borrow = Borrow {
+    bookTitle :: String,
+    author :: String,
+    loanDate :: Day,
+    returnDate :: Maybe Day,
+    memberInfo :: Member
     } deriving (Show)
 
--- Sample Data
+type Members = Map.Map Int Member
+type Borrows = [Borrow]
 
-type MembersDB = Map Int Member
-type LoanDB = [LoanRecord]
+data Library = Library {
+    members :: Members,
+    loanRecords :: Borrows
+} deriving (Show)
 
-data LibrarySystem = LibrarySystem
-    { members :: MembersDB
-    , loans :: LoanDB
-    } deriving (Show)
+registerMember :: Library  -> String -> MembershipType -> String -> Library 
+registerMember library memName memType memEmail =
+    let memId = Map.size (members library) + 1
+        member = Member memId memName memType memEmail
+    in library {members = Map.insert memId member (members library)}
+    
+    
+borrowBook :: Library -> String -> String -> Day -> Int -> Either String Library
+borrowBook library bookTitle bookAuthor loanDate memId = 
+            case Map.lookup memId (members library) of
+                Nothing -> Left "Couldn't find the member"
+                Just member -> 
+                    let memberLoans = filter ((==memId) . memberId . memberInfo) (loanRecords library)
+                        maxLoans = case membershipType member of
+                            Standard -> 3
+                            Premium -> 6
+                    in if length memberLoans >= maxLoans 
+                        then Left "Borrowing limit exceeded"
+                        else let bookToBorrow = Borrow bookTitle bookAuthor loanDate Nothing member
+                            in Right library { loanRecords = bookToBorrow : loanRecords library }
+             
+                            
+returnBook :: Library -> String -> Day -> Library
+returnBook library title date =
+    let updatedLoans = map updateLoan (loanRecords library)
+        updateLoan borrow =
+            if bookTitle borrow == title && isNothing (returnDate borrow)
+                then borrow { returnDate = Just date}
+                else borrow
+    in library { loanRecords = updatedLoans } 
+        
 
--- Functions
+checkLoanStatus :: Library -> String -> String
+checkLoanStatus library title = 
+    let loan = filter ((==title) . bookTitle) (loanRecords library) 
+    in case loan of
+        [] -> "Book is available"
+        selectedBook -> if isNothing (returnDate (head selectedBook)) then "Book is not available"
+             else "Book is available"
 
--- 1. Register Member
-registerMember :: LibrarySystem -> String -> MembershipType -> String -> LibrarySystem
-registerMember system memberName mType memberEmail =
-    let newId = if Map.null (members system) then 1 else fst (Map.findMax (members system)) + 1
-        newMember = Member newId memberName mType memberEmail
-    in system { members = Map.insert newId newMember (members system) }
-
--- 2. Borrow Book
-borrowBook :: LibrarySystem -> String -> String -> Day -> Int -> Either String LibrarySystem
-borrowBook system title author date mId =
-    --member validation
-  case Map.lookup mId (members system) of
-    Nothing -> Left "Invalid Member ID."
-    Just member ->
-      let memberLoans = filter ((== mId) . memberId . memberInfo) (loans system)
-          maxLoans = case membershipType member of
-            Standard -> 3
-            Premium -> 5
-      in if length memberLoans >= maxLoans
-         then Left "Borrowing limit exceeded."
-         else Right system { loans = LoanRecord title author date Nothing member : loans system }
-
--- 3. Return Book
-returnBook :: LibrarySystem -> String -> Day -> LibrarySystem
-returnBook system title returnDate =
-  let updatedLoans = map updateLoan (loans system)
-      updateLoan loan =
-        if bookTitle loan == title && isNothing (returnDate loan)
-        then loan { returnDate = Just returnDate }
-        else loan
-  in system { loans = updatedLoans }
+applyMembershipDiscount :: Either String Library -> Int -> Double -> String 
+applyMembershipDiscount (Right library) mId fee = 
+    case Map.lookup mId (members library) of
+        Nothing -> "Member not found"
+        Just member -> 
+            let discountList = Map.fromList [(Standard, 0.0), (Premium, 0.25)]
+                discount = case Map.lookup (membershipType member) discountList of
+                             Nothing -> 0 
+                             Just d -> d 
+            in "discounted price: " ++ (show (fee * (1 - discount)))
+applyMembershipDiscount (Left a) _ _ = a 
 
 
--- 4. Check Loan Status
-checkLoanStatus :: LibrarySystem -> String -> String
-checkLoanStatus system title =
-  case filter ((== title) . bookTitle) (loans system) of
-    [] -> "Book is available."
-    records -> if any (isNothing . returnDate) records
-               then "Book is currently borrowed."
-               else "Book is available."
 
--- 5. Apply Membership Discount
-applyDiscount :: LibrarySystem -> Int -> Double -> Either String Double
-applyDiscount system mId fee =
-  case Map.lookup mId (members system) of
-    Nothing -> Left "Invalid Member ID."
-    Just member ->
-      let discountMap = Map.fromList [(Standard, 0.0), (Premium, 0.2)]
-      in Right $ fee * (1 - Map.findWithDefault 0 (membershipType member) discountMap)
+totalActiveMembers :: Library -> Int
+totalActiveMembers library = length $ Map.elems $ members library
 
--- 6. Generate Membership Statistics
-generateStatistics :: LibrarySystem -> (Int, Double, [String])
-generateStatistics system =
-  let activeMembers = length (Map.elems $ members system)
-      totalBooksBorrowed = fromIntegral $ length (loans system)
-      avgBooksPerMember = totalBooksBorrowed / fromIntegral activeMembers
-      bookFrequency = map head . sortOn (negate . length) . group . map bookTitle $ loans system
-   in (activeMembers, avgBooksPerMember, take 3 bookFrequency)
 
--- Higher-order functions for filtering and mapping data
-filterLoansByMember :: Int -> LoanDB -> LoanDB
-filterLoansByMember mId = filter ((== mId) . memberId . memberInfo)
+averageBookBorrowedByMember :: Library -> Double
+averageBookBorrowedByMember library =
+    let totalBorrows = length (loanRecords library)
+        memberCount = Map.size (members library)
+    in if memberCount == 0 then 0 else fromIntegral totalBorrows / fromIntegral memberCount
 
--- Error handling and Functor usage
-safeMemberLookup :: Int -> MembersDB -> Either String Member
-safeMemberLookup mId db = maybe (Left "Member not found.") Right (Map.lookup mId db)
 
--- Usage example
+mostPopularBooks :: Library -> [(String, Int)]
+mostPopularBooks library =
+    let borrowCounts = foldr (\borrow acc -> Map.insertWith (+) (bookTitle borrow) 1 acc) Map.empty (loanRecords library)
+    in Map.toList $ Map.filter (> 1) borrowCounts 
+
+
+
+
 main :: IO ()
 main = do
-  let initialSystem = LibrarySystem { members = Map.empty, loans = [] }
-  let updatedSystem = registerMember initialSystem "Alice" Premium "alice@example.com"
-  print $ members updatedSystem
+    currentTime <- getCurrentTime
+    let formattedTime = utctDay currentTime
+    
+    let initLib = Library Map.empty []
+    let mem1 = registerMember initLib "Ariq" Premium "Ariq@gmail.com"
+    let mem2 = registerMember mem1 "Kassiyet" Standard "Kassiyet@gmail.com"
+    let mem3 = registerMember mem2 "Amir" Premium "Amir@gmail.com"
+
+    let loan1 = borrowBook mem3 "Harry Potter" "J.K. Rolling" (read "2024-11-15") 1
+    let loan2 = loan1 >>= (\x -> borrowBook x "Lord Of the Ring" "J.R.R Tolkein" (read "2024-10-21") 1)
+    let loan3 = loan2 >>= (\x -> borrowBook x "The Great Gatsby" "F Scott Fitzgerald" (read "2024-11-13") 1)
+    let loan4 = loan3 >>= (\x -> borrowBook x "The Catcher In The Rye" "J.D. Salinger" (read "2024-11-10") 2)
+    let loan5 = loan4 >>= (\x -> borrowBook x "1984" "George Orwell" (read "2024-9-9") 2)
+    let loan6 = loan5 >>= (\x -> borrowBook x "To Kill a Mockingbird" "Harper Lee" (read "2024-11-14") 2)
+    let loan7 = loan6 >>= (\x -> borrowBook x "The Great Gatsby" "F Scott Fitzgerald" (read "2024-11-17") 3)
+
+
+    let return1 = fmap (\x -> returnBook x "The Great Gatsby" (read "2024-11-15")) loan7
+    let return2 = fmap (\x -> returnBook x "To Kill a Mockingbird" (read "2024-11-18")) return1
+
+
+
+    let check = fmap (\x -> checkLoanStatus x "Harry Potter") return2
+    print ( (\x -> "check if Harry Potter is available: " ++ x) <$> check )
+
+    let discount = applyMembershipDiscount return2 1 600
+    print $ discount ++ " (original price is 600)"
+
+    let activeMembers = fmap (\x -> totalActiveMembers x) return2
+    print ( (\x -> "total active members: " ++ show x) <$> activeMembers)
+
+    let average_book_borrowed_by_member = fmap (\x -> averageBookBorrowedByMember x) return2
+    print ( (\x -> "average book borrowed by members: " ++ show x) <$> average_book_borrowed_by_member)
+    
+    let most_popular_books = fmap (\x -> mostPopularBooks x) return2
+    print ( (\x -> "most popular books: " ++ show x) <$> most_popular_books)
